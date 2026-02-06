1779e806c5b73dd94a628639b094fc07630aebb6
Shaurya Singh <honourableshaurya@gmail.com>
2026-02-06 20:00:30 +0530

    Add files via upload

---
+++ day05.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int p, q;
+
+    // Read sizes
+    scanf("%d", &p);
+    int a[p];
+
+    for(int i = 0; i < p; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &q);
+    int b[q];
+
+    for(int i = 0; i < q; i++)
+        scanf("%d", &b[i]);
+
+    int i = 0, j = 0;
+
+    // Merge using two pointers
+    while(i < p && j < q) {
+        if(a[i] <= b[j]) {
+            printf("%d ", a[i]);
+            i++;
+        } else {
+            printf("%d ", b[j]);
+            j++;
+        }
+    }
+
+    // Print remaining elements (if any)
+    while(i < p) {
+        printf("%d ", a[i]);
+        i++;
+    }
+
+    while(j < q) {
+        printf("%d ", b[j]);
+        j++;
+    }
+
+    return 0;
+}
