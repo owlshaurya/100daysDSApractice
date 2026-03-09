4ab0380aadbed6beff1589b0bdc443f3b3338f4d
Shaurya Singh <honourableshaurya@gmail.com>
2026-03-09 23:46:53 +0530

    Add files via upload

---
+++ day35.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = -1, rear = -1;
+
+void enqueue(int value) {
+    if(rear == MAX - 1) {
+        printf("Queue Overflow\n");
+        return;
+    }
+
+    if(front == -1)
+        front = 0;
+
+    queue[++rear] = value;
+}
+
+void display() {
+    for(int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+}
+
+int main() {
+    int n, value;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        enqueue(value);
+    }
+
+    display();
+
+    return 0;
+}
